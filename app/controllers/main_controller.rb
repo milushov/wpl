c1a64af8df9ef8dd73b614d78314d9b9bb4741ad
Roman Milushov <roma@milushov.ru>
2012-04-12 03:31:21 +0400

    seed.rb

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,78 +1,80 @@
 class MainController < ApplicationController
-	require 'open-uri'
-	require 'openssl'
+  require 'open-uri'
+  require 'openssl'
 
-	def index
-		if isAuth?
-			# сдесь будем выбирать профиль пользователя по vk_id
-			@user_profile = getProfile(session[:user_id])
+  def index
+    if isAuth?
+      # сдесь будем выбирать профиль пользователя по vk_id
+      @user_profile = getProfile(session[:user_id])
+      unless @user_profile
+        render 'main/start' and return
+      end
+      #@playlists = Playlist.all
+      @playlists = @user_profile[:playlists]
+      # auth_key будет посылать в хедаре при каждом запросе к апи
+      @auth_key = Digest::MD5.hexdigest( "#{session[:user_id]}_roma_123_super_salt" )
+      respond_to do |format|
+        format.html # index.html.haml
+      end
+    else
+      render 'main/start'
+    end
+  end
 
-			#@playlists = Playlist.all
-			@playlists = @user_profile[:playlists]
-			# auth_key будет посылать в хедаре при каждом запросе к апи
-			@auth_key = Digest::MD5.hexdigest( "#{session[:user_id]}_roma_123_super_salt" )
-			respond_to do |format|
-				format.html # index.html.haml
-			end
-		else
-			render 'main/start'
-		end
-	end
+  def login
+    if session[:access_token].nil? or session[:user_id].nil?
+      requestAuth params[:return_to]
+    else
+      redirect_to action: 'index'
+    end
+  end
 
-	def login
-		if session[:access_token].nil? or session[:user_id].nil?
-			requestAuth params[:return_to]
-		else
-			redirect_to action: 'index'
-		end
-	end
+  def logout
+    cookies[:access_token] = session[:access_token] = nil
+    cookies[:user_id] = session[:user_id] = nil
+    redirect_to action: 'index'
+  end
 
-	def logout
-		cookies[:access_token] = session[:access_token] = nil
-		cookies[:user_id] = session[:user_id] = nil
-		redirect_to action: 'index'
-	end
+  # получает код для получения токена
+  def auth(return_to = nil)
+    if params[:code].nil?
+      redirect_to action: 'index'
+    elsif params[:error] and params[:error_description]
+      render inline: "#{params[:error]} - #{params[:error_description]}" and return
+    end
 
-	# получает код для получения токена
-	def auth(return_to = nil)
-		if params[:code].nil?
-			redirect_to action: 'index'
-		elsif params[:error] and params[:error_description]
-			render inline: "#{params[:error]} - #{params[:error_description]}" and return
-		end
+    uri = "https://oauth.vk.com/access_token?client_id=#{ APP_ID }&client_secret=#{ APP_SECRET }&code=#{params[:code]}"
 
-		uri = "https://oauth.vk.com/access_token?client_id=#{ APP_ID }&client_secret=#{ APP_SECRET }&code=#{params[:code]}"
+    begin
+      response = open(uri, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE).read
+    rescue => ex
+      render inline: "#{ex.class}: #{ex.message} uri(#{uri}) <b>failed</b>" and return
+    end
 
-		begin
-			response = open(uri, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE).read
-		rescue => ex
-			render inline: "#{ex.class}: #{ex.message} uri(#{uri}) <b>failed</b>" and return
-		end
+    response = JSON.parse response
 
-		response = JSON.parse response
+    if response[:error] and response[:error_description]
+      render inline: "#{params[:error]} - #{params[:error_description]}" and return
+    else
+      saveToken response['access_token'], response['user_id']
+      if params[:return_to]
+        redirect_to "/#{params[:return_to]}"
+      else
+        redirect_to action: 'index'
+      end
+    end
+  end
 
-		if response[:error] and response[:error_description]
-			render inline: "#{params[:error]} - #{params[:error_description]}" and return
-		else
-			saveToken response['access_token'], response['user_id']
-			if params[:return_to]
-				redirect_to "/#{params[:return_to]}"
-			else
-				redirect_to action: 'index'
-			end
-		end
-	end
+  private
 
-	private
+  # запрашивает код
+  def requestAuth(return_to = nil)
+    redirect_uri = return_to ? "#{REDIRECT_URI}?return_to=#{return_to}" : REDIRECT_URI
+    redirect_to "http://oauth.vk.com/authorize?client_id=#{ APP_ID }&scope=#{ SETTINGS }&redirect_uri=#{ redirect_uri }&response_type=code"
+  end
 
-	# запрашивает код
-	def requestAuth(return_to = nil)
-		redirect_uri = return_to ? "#{REDIRECT_URI}?return_to=#{return_to}" : REDIRECT_URI
-		redirect_to "http://oauth.vk.com/authorize?client_id=#{ APP_ID }&scope=#{ SETTINGS }&redirect_uri=#{ redirect_uri }&response_type=code"
-	end
-
-	def saveToken( access_token, user_id )
-		@app.access_token = cookies[:access_token] = session[:access_token] = access_token
-		cookies[:user_id] = session[:user_id] = user_id
-	end
+  def saveToken( access_token, user_id )
+    @app.access_token = cookies[:access_token] = session[:access_token] = access_token
+    cookies[:user_id] = session[:user_id] = user_id
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+playlists = JSON.parse File.open("#{Rails.root}/db/playlists.json").read.to_s
+
+Playlist.delete_all and User.delete_all
+
+created_playlists = []
+
+p 'creating playlists...'
+playlists.each do |playlist|
+  created_playlists << Playlist.create!(playlist)
+  p "create playlist - #{playlist["name"]}"
+end
+
+p 'creating users...'
+users = File.open("#{Rails.root}/db/users.txt") do |users|
+  users.read.each_line do |user|
+    vk_id, followee = user.split('|')
+    new_user = User.create!(vk_id: vk_id)
+    p "user #{new_user.id.to_s[-3,3]} created"
+  end
+end
+
+users = File.open("#{Rails.root}/db/users.txt") do |users|
+  users.read.each_line do |user|
+    vk_id, followee = user.split('|')
+    user_action = User.where({vk_id: vk_id}).first
+
+    created_playlists.each do |playlist|
+      if (rand(1..100).even?)
+        user_action.follow( playlist )
+        p "user #{user_action.id.to_s[-3,3]} followed playlist #{playlist.id.to_s[-3,3]}"
+      end
+    end
+    
+    followee.split(' ').each do |id|
+      if (rand(1..100).even?)
+        who_follow = User.where({vk_id: id}).first
+        user_action.follow( who_follow )
+        p "user #{user_action.id.to_s[-3,3]} followed user #{who_follow.id.to_s[-3,3]}"
+      end
+    end
+  end
+end
