60f7b4deabb2913f1601345060f7130105b3b504
Roman Milushov <github@milushov.ru>
2012-09-16 12:00:53 +0400

    added logger for omni-auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   APP_ID = ENV['USER'] ? 2999165 : 1111000 # development and production app id
   APP_SECRET = '1111000key'
